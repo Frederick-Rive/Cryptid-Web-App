90a7b10f85973fe2548c0ffa6348d1c686e377ea
theDragun <western.jake.morris@gmail.com>
2022-05-09 14:53:44 +1200

    placeholder login inputs added if needed. example map marker added to JS.

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,13 +1,16 @@
 //creating the map
-var map = L.map('Mapid').setView([0, 0], 13);
+var map = L.map('Mapid').setView([-41.2924, 174.7787], 13);
 
 //adding a tile layer to the map
 var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
+	maxZoom: 16,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
-// NOTE: Work in progress
+// adding the tile map to the map
 var Layer = OpenStreetMap_Mapnik
-
 Layer.addTo(map);
+
+// NOTE: Marker Example
+var testmark = L.marker([-41.2924, 174.7787]).bindPopup('I AM A MASSIVE TEST').addTo(map);
+testmark.addTo(map);
