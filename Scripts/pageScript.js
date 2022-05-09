22f4eac6c1b255670109408199bdd628f422e00f
theDragun <western.jake.morris@gmail.com>
2022-05-09 16:00:55 +1200

    added button event listeners

---
+++ Scripts/pageScript.js
@@ -0,0 +1,10 @@
+
+//getting buttons
+var loginButton = getElementsByName('loginButton');
+var registerButton = getElementsByName('registerButton');
+var searchConfirm = getElementsByName('searchConfirm');
+
+//button event listeners
+loginButton.addeventlistener();
+registerButton.addeventlistener();
+searchConfirm.addeventlistener();
